64ae2b41051c741e1a01856a9ff02c6fbfc4229f
aaqadir <aqansari47@gmail.com>
2023-08-10 20:20:51 +0530

    Started working on Frontend

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiUrl: string = '';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public getEmployee(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.apiUrl}/employee/all`);
+  }
+
+  public getEmployeeById(employeeId: number): Observable<void> {
+    return this.httpClient.get<void>(
+      `${this.apiUrl}/employee/find${employeeId}`
+    );
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.post<Employee>(
+      `${this.apiUrl}/employee/add`,
+      employee
+    );
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.put<Employee>(
+      `${this.apiUrl}/employee/update`,
+      employee
+    );
+  }
+
+  public deleteEmployeeById(employeeId: number): Observable<void> {
+    return this.httpClient.delete<void>(
+      `${this.apiUrl}/employee/delete${employeeId}`
+    );
+  }
+}
